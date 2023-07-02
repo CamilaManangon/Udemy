d769891a39818112047c6f059e8c14794b340260
CamilaManangon <camila.manangon14@gmail.com>
2023-07-02 00:22:06 -0500

    1

---
+++ Practica 5/Practica5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    float nota;
+    printf("Ingrese le nota del alumno: ");
+    scanf("%f", &nota);
+    if(nota<5){
+        printf("Suspenso\n");
+    }else if(nota == 6){
+        printf("Bien\n");
+    }else if(nota == 7 || nota == 8){
+        printf("Notable\n");
+    }else if(nota == 9 || nota == 10){
+        printf("Sobresaliente\n");
+    }else{
+        printf("Nota invalida\n");
+    }
+}
