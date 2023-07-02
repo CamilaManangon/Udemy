4ebf0d715bfb94829e28bf2a22ef76500605746c
CamilaManangon <camila.manangon14@gmail.com>
2023-07-02 00:18:08 -0500

    1

---
+++ Practica 4/Practica4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    float euro;
+    printf("Ingrese la cantidad de euros que quiera convertir: ");
+    scanf("%f",&euro);
+
+    printf("%.2f euros equivale a %.2f pesetas.\n",euro, euro*166.386);
+
+
+}
